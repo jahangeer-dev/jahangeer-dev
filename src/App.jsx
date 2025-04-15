a548c3562cb1848e972d2c96e601da05640e8c83
jahangeer7704 <jahangeer7704@gmail.com>
2025-04-15 15:43:20 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -34,30 +34,7 @@ const App = () => {
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern min-h-screen bg-cover bg-no-repeat bg-center">
           <Navbar />
-          {
-            windowWidth >1000 ? <Hero/>:(<div
-                    className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
-                  >
-                    <div className="flex flex-col justify-center items-center mt-5">
-                      <div className="w-5 h-5 rounded-full bg-[#915eff]" />
-                      <div className="w-1 sm:h-80 h-40 violet-gradient" />
-                    </div>
-                    <div>
-                      <h1 className={`${styles.heroHeadText} text-white`}>
-                        Hi, I'm <span className="text-[#915eff]">Jahangeer</span>
-                      </h1>
-                      <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-                        Passionate about building scalable web applications with modern technologies.
-                      </p>
-                      <div className="h-fit  flex justify-center items-center ">
-                        <img src="/Hari.png" alt="" className=" rounded-full  shadow-2xl mix-blend-lighten" />
-                        
-                      </div>
-                    </div>
-                  </div>)
-          }
-         
-        
+          <Hero/>
         </div>
         <About />
         <Experience />
